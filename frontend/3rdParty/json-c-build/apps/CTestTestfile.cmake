b31d1171a09aea7ed5a7248242afced59d889692
lcazzulo <lcazzulo@igate.it>
2024-05-25 22:32:39 +0200

    Delete frontend/3rdParty/json-c-build directory

--- frontend/3rdParty/json-c-build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/emdc/EMDC/3rdParty/json-c/apps
-# Build directory: /home/emdc/EMDC/3rdParty/json-c-build/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
