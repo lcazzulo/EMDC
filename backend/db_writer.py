b2ff3018a016472f04f59c2ca9c1a1feceddfda2
Luca Cazzulo <lcazzulo@igate.it>
2024-06-05 18:06:30 +0200

    Gestita disconnessione da broker per programmi python.

--- backend/db_writer.py
+++ backend/db_writer.py
@@ -19,23 +19,43 @@ def callback(ch, method, properties, body):
 
 def run_app():
 
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='altair.luca-cazzulo.me'))
-    channel = connection.channel()
+    while(True):
+        try:
+
+            connection = pika.BlockingConnection(pika.ConnectionParameters(host='altair.luca-cazzulo.me'))
+            channel = connection.channel()
 
 
-    #result = channel.queue_declare(queue='queue-1', exclusive=False)
-    #queue_name = result.method.queue
-    queue_name = 'queue-1'
-    #channel.queue_bind(exchange='EMDC', queue=queue_name, routing_key="A.B.C")
+            #result = channel.queue_declare(queue='queue-1', exclusive=False)
+            #queue_name = result.method.queue
+            queue_name = 'queue-1'
+            #channel.queue_bind(exchange='EMDC', queue=queue_name, routing_key="A.B.C")
 
-    print(' [*] Waiting for logs. To exit press CTRL+C')
-
-
+            print(' [*] Waiting for logs. To exit press CTRL+C')
 
 
-    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
-    channel.start_consuming()
+
+            channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+            try:
+                channel.start_consuming()
+            except KeyboardInterrupt:
+                channel.stop_consuming()
+                connection.close()
+                break
+        except pika.exceptions.ConnectionClosedByBroker:
+            time.sleep(10)
+            continue
+        # Do not recover on channel errors
+        except pika.exceptions.AMQPChannelError as err:
+            print("Caught a channel error: {}, stopping...".format(err))
+            break
+        # Recover on all other connection errors
+        except pika.exceptions.AMQPConnectionError:
+            print("Connection was closed, retrying...")
+            time.sleep(10)
+            continue
+    
 
 if __name__ == '__main__':
     run_app()
