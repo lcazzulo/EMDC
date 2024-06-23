75596f0a82a1babc5b46a78a2e214a975f0ade8e
Luca Cazzulo <lcazzulo@igate.it>
2024-06-23 11:07:16 +0200

    Aggiunta stampa timestamp a utils/power_report.py

--- backend/utils/power_report.py
+++ backend/utils/power_report.py
@@ -12,15 +12,15 @@ def run_app():
         password="emdc",
         database="EMDC")
     cur = db_conn.cursor()
-    query = "select sample_ts from samples where rarr_flag = 0 order by sample_ts desc limit 10;"
+    query = "select FROM_UNIXTIME(SAMPLE_TS/1000, '%Y-%m-%d %H:%i:%s') as TIMESTAMP, sample_ts from samples where rarr_flag = 0 order by sample_ts desc limit 41;"
     cur.execute(query)
-    for (sample_ts,) in cur:
+    for (timestamp, sample_ts) in cur:
         if (last_val == 0):
             last_val = sample_ts
         else:
             diff = last_val - sample_ts
             last_val = sample_ts
-            print(round(3600 / diff, 3))
+            print(timestamp + " " + str(round(3600 / diff, 3)) + " kW")
 
 
 if __name__ == '__main__':
